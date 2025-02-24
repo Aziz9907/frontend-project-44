911bfabcdcd57c63bb0aab534a1a34abb51efebe
Aziz9907 <azizbekkomilzonov22@gmail.com>
2025-02-24 11:31:40 +0700

    Delete src directory

--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const game = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('Please enter your name: ');
-  console.log(`Hello, ${name}!`);
-};
-
-export default game;
