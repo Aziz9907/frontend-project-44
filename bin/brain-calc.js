4cae98f43e649d082a7fa90f242e22ea06385585
aziz <saldal228@gmail.com>
2025-03-04 12:13:22 +0700

    changes4

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -5,38 +5,49 @@ import game from '../src/cli.js';
 const name = game();
 
 console.log('What is the result of the expression?');
+const operations = ['+', '-', '*'];
 
-const generateExpression = () => {
-  const getRandomInt1 = () => Math.floor(Math.random() * 50) + 1;
-  const getRandomInt2 = () => Math.floor(Math.random() * 50) + 1;
-  const operations = ['+', '-', '*'];
-  const operation = operations[Math.floor(Math.random() * operations.length)];
+// const generateExpression = () => {
+//   const getRandomInt1 = () => Math.floor(Math.random() * 50) + 1;
+//   const getRandomInt2 = () => Math.floor(Math.random() * 50) + 1;
 
-  const expression = `${getRandomInt1} ${operation} ${getRandomInt2}`;
-  return expression;
-};
+//   const operation = operations[Math.floor(Math.random() * operations.length)];
+//   return operation;
+// };
 
-const calculateExpression = (num1, num2, operation) => {
-  switch (operation) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    default:
-      return null;
-  }
-};
-let sum = 0;
+// const calculateExpression = (num1, num2, operation) => {
+//   switch (operation) {
+//     case '+':
+//       return num1 + num2;
+//     case '-':
+//       return num1 - num2;
+//     case '*':
+//       return num1 * num2;
+//     default:
+//       return null;
+//   }
+// };
+// let sum = 0;
 
-const calcGame = () => {
-  do {
-    const { num1, num2, operation } = generateExpression();
-    const correctAnswer = calculateExpression(num1, num2, operation);
-    const expression = `${num1} ${num2} ${operation}`;
-    console.log(`Question is: ${expression}`);
-    readlineSync.question('Your answer is: ');
-  } while (sum < 3);
-};
-calcGame();
+// const calcGame = () => {
+//   do {
+//     const { num1, num2, operation } = generateExpression();
+//     const correctAnswer = calculateExpression(num1, num2, operation);
+//     const expression = `${num1} ${num2} ${operation}`;
+//     console.log(`Question is: ${expression}`);
+//     readlineSync.question('Your answer is: ');
+//   } while (sum < 3);
+// };
+// calcGame();
+
+function generateExpression(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const calculateExpression = {
+  '+': function (x, y) { return x + y },
+  '-': function (x, y) { return x - y },
+  '*': function (x, y) { return x * y }
+}
