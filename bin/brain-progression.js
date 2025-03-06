377e86e7e51a0db105da5572762a91567eede727
Your Name <you@example.com>
2025-03-07 05:32:38 +0700

    changes6

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import game, {generateRandNum} from '../src/cli.js';
+import game, { generateRandNum } from '../src/cli.js';
+
+const name = game();
+
+const arr = [];
+function progressionGame() {
+  const length = generateRandNum(5, 10);
+  const num1 = generateRandNum();
+  const steps = generateRandNum();
+  for (let i = 0; i < length; i += 1) {
+    arr.push(num1 + steps + i);
+  }
+  console.log(arr.join(' '));
+}
+
+progressionGame();
