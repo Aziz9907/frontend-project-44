3930638bfc3a032d8540f6bd3456d2fef2753f16
Aziz <azizbekkomilzonov22@gmail.com>
2025-03-10 13:32:19 +0700

    changes7

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import game, { generateRandNum, isPrime } from '../src/cli.js';
+import game, { generateRandNum } from '../src/cli.js';
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
 
 const gamePrime = () => {
   const name = game();
